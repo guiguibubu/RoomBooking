c74f4e314052f03d96f7146f1c6d3c740581d13f
Pphttp <p.pierre.35@gmail.com>
2018-01-09 11:44:46 +0100

    Squelette du client et import librairies

--- client/process.js
+++ client/process.js
@@ -1,1 +1,42 @@
 // Javascript functions
+let app3, app4, app7;
+
+$(document).ready(function(){
+
+    app3 = new Vue({
+        el: '#app-3',
+        data: {
+            seen: true
+        }
+    });
+
+    app4 = new Vue({
+        el: '#app-4',
+        data: {
+            todos: [
+                { text: 'Learn JavaScript' },
+                { text: 'Learn Vue' },
+                { text: 'Build something awesome' }
+            ]
+        }
+    });
+
+    Vue.component('todo-item', {
+        props: ['todo'],
+        template: '<li><b>{{ todo.text }}</b></li>'
+    });
+
+    app7 = new Vue({
+        el: '#app-7',
+        data: {
+            groceryList: [
+                { id: 0, text: 'Vegetables' },
+                { id: 1, text: 'Cheese' },
+                { id: 2, text: 'Whatever else humans are supposed to eat' }
+            ]
+        }
+    });
+
+
+});
+
