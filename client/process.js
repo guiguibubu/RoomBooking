64e3d11e0143e5c36f72ee837b4fceca36b6bcf9
Martin Tulard <martin.tulard@gmail.com>
2018-01-13 18:36:11 +0100

    Ajout pagination sur la liste des salles

--- client/process.js
+++ client/process.js
@@ -1,4 +1,4 @@
-// Javascript functions
+// Javascript
 let home;
 let loadedData = [
     {
@@ -9,28 +9,16 @@ let loadedData = [
     },
     {
         id: 1,
-        name: "Nom de la salle de test",
-        number: "A-4786",
-        comment: "gauche"
-    },
-    {
-        id: 2,
-        name: "Nom de la salle de testeuh",
-        number: "B-756",
-        comment: "couloir"
-    },
-    {
-        id: 3,
-        name: "heisenedber de la salle",
-        number: "C-14",
-        comment: "Au fond"
+        name: "test",
+        number: "2",
+        comment: "test"
     }
 ];
 
 // Exécute un appel AJAX GET
 // Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
 function ajaxGet(url, callback) {
-    var req = new XMLHttpRequest();
+    let req = new XMLHttpRequest();
     req.open("GET", url);
     req.addEventListener("load", function () {
         if (req.status >= 200 && req.status < 400) {
@@ -46,38 +34,66 @@ function ajaxGet(url, callback) {
     req.send(null);
 }
 
-function fillTab(responseText) {
+// Creates the vue with the data from "loadedData" and it pagination
+function fillRoomList(responseText) {
     loadedData = JSON.parse(responseText);
     home = new Vue({
         el: '#room-list',
         data: {
-            roomList: loadedData
+            roomList: []
         }
     });
+
+    $('#pagination-container').pagination({
+        dataSource: loadedData,
+        pageSize: 5,
+        showPrevious: true,
+        showNext: true,
+        className: 'paginationjs-theme-blue',
+        callback: function (data, pagination) {
+            home.roomList = data;
+        }
+    })
 }
 
+$().ready(function () {
 
-
-
-$(document).ready(function () {
-
+    /* Vue component defining a room :
+     * takes a room object as prop
+     * @todo: manage case where info is missing (especially with the dots between words)
+     * @todo: link icons to a value from the room, color grading and/or differents icons
+     * @todo: "reserver" button not implemented
+     * This template is used for every room in the room list
+     */
     Vue.component('room-item', {
         props: ['room'],
-        template:
-        '<div class="room">' +
+        template: '<div class="room">' +
             '<div class="room-infos">' +
-                '<span class="room-name">{{room.name}}</span>' +
-                '<span class="room-location">{{room.location}} • Bâtiment {{room.building }} • Étage {{room.floor}} • {{room.comment}}</span>' +
-                '<span class="room-type">{{room.capacity === null ? 34 : 12 }} Personnes • {{room.type === null ? "Laboratoire informatique" : "Amphitéatre"}} </span>' +
+            '<span class="room-name">{{room.name}}</span>' +
+            '<span class="room-location">{{room.location}} • Bâtiment {{room.building }} • Étage {{room.floor}} • {{room.comment}}</span>' +
+            '<span class="room-type">{{room.capacity === null ? 34 : 12 }} Personnes • {{room.type === null ? "Laboratoire informatique" : "Amphitéatre"}} </span>' +
             '</div>' +
             '<div class="room-icons">' +
-                '<i class="room-wifi fa fa-wifi fa-lg" aria-hidden="false"></i>' +
-                '<i class="room-video fa fa-object-group fa-lg" aria-hidden="false"></i>'+
-                '<button class="room-book"> Réserver</button>'+
-        '   </div>' +
-        '</div>'
+            '<i class="room-wifi fa fa-wifi fa-lg" aria-hidden="false"></i>' +
+            '<i class="room-video fa fa-object-group fa-lg" aria-hidden="false"></i>' +
+            '<button class="room-book"> Réserver</button>' +
+            '   </div>' +
+            '</div>'
     });
 
-    ajaxGet("http://vps316698.ovh.net/api/rooms", fillTab);
+
+    // Function getting the data from the server, hiding buttons, putting on top title and showing room list
+    $("#btn-now").click(function () {
+        ajaxGet("http://vps316698.ovh.net/api/rooms", fillRoomList);
+        $("#home").css("margin-top", "0%");
+        $("#home-btn").hide();
+        $("#room-list").show();
+    });
+
+
+    // Work in progress
+    $("#btn-later").click(function () {
+        alert("J'ai pas fini de faire ça non mais oh !")
+    })
 
 });
