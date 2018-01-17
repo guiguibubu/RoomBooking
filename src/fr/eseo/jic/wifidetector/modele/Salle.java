6652c5b2bb47ee9d7b6f458bb0851b9f17cf377f
Lilian Braud <lilian.braud@reseau.eseo.fr>
2018-01-17 13:38:19 +0100

    Test de calculListePointMesure()

--- src/fr/eseo/jic/wifidetector/modele/Salle.java
+++ src/fr/eseo/jic/wifidetector/modele/Salle.java
@@ -11,38 +11,39 @@ public class Salle extends SurfaceRectangulaire {
 	private List<ZoneMesure> listeZoneMesure;
 
 	/**
-	 * Diamètre pris en compte pour connaitre le nombre de points de mesure à faire (en mètre)
+	 * Diamï¿½tre pris en compte pour connaitre le nombre de points de mesure ï¿½ faire (en mï¿½tre)
 	 */
-	public static final int DIAMETRE_MESURE = 10;
+	public static final double DIAMETRE_MESURE = 10;
 
 	/*CONSTRUCTEURS*/
 	public Salle(int largeur, int hauteur) throws Exception{
 		super(largeur, hauteur);
 		this.listeZoneMesure = new ArrayList<ZoneMesure>();
+		calculListePointMesure();
 	}
 
-	public Salle() throws Exception{
+	public Salle() throws Exception {
 		this(0,0);
 	}
 
-	public void calculListePointMesure() throws Exception{
-		int nbPointMesureLargeur = this.getLargeur() / DIAMETRE_MESURE;
-		int nbPointMesureHauteur = this.getHauteur() / DIAMETRE_MESURE;
+	public void calculListePointMesure() throws Exception {
+		int nbPointMesureLargeur = (int) Math.ceil(this.getLargeur() / DIAMETRE_MESURE);
+		int nbPointMesureHauteur = (int) Math.ceil(this.getHauteur() / DIAMETRE_MESURE);
 		int nbPointMesure = nbPointMesureLargeur*nbPointMesureHauteur;
 
 		int largeurMesure = this.getLargeur() / nbPointMesureLargeur;
 		int hauteurMesure = this.getHauteur() / nbPointMesureHauteur;
 
-		for(int i = 0; i<nbPointMesure; i++){
-			int indexLigne = i / nbPointMesureLargeur;
-			int indexColonne = i / nbPointMesureHauteur;
-			int xZoneMesure = indexColonne*largeurMesure;
-			int yZoneMesure = indexLigne*hauteurMesure;
-			this.getListeZoneMesure().add(new ZoneMesure(xZoneMesure, yZoneMesure, largeurMesure, hauteurMesure));
+		for(int i = 0; i<nbPointMesureLargeur; i++){
+			for(int j = 0; j<nbPointMesureHauteur; j++){
+				int indexLigne = i;
+				int indexColonne = j;
+				int xZoneMesure = indexColonne*largeurMesure;
+				int yZoneMesure = indexLigne*hauteurMesure;
+				this.getListeZoneMesure().add(new ZoneMesure(xZoneMesure, yZoneMesure, largeurMesure, hauteurMesure));
+			}
 		}
 	}
-
-	/*GETTER SETTER*/
 
 	public List<ZoneMesure> getListeZoneMesure() {return this.listeZoneMesure;}
 	public void setListeZoneMesure(List<ZoneMesure> listeZoneMesure) {this.listeZoneMesure = listeZoneMesure;}
